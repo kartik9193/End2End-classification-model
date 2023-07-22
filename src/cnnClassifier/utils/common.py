6cf3be71f38822f5ddfc32267b544d2e3210e3f6
Kartik Nerwal <kartik9193@gmail.com>
2023-07-22 10:13:56 +0530

    changes made in various modules

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -10,3 +10,71 @@ from pathlib import Path
 from typing import Any
 import base64
 
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """read yaml file and returns
+    
+    Args:
+        path_to_yaml (str): path like input
+        
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+    
+    Return:
+        ConfigBox: ConfigBox type
+    """
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+    
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"create directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+    
+    Args:
+    path (Path): path to json file
+    dict (dict): data to be saved is json
+    """
+
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+def decodeImage(imgstring, fileName):
+    imagdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgstring)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+
+
